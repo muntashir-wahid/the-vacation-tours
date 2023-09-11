40d4279f17f6d3313732730ae7fb1587ca2c5600
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-09-11 09:48:54 +0600

    used aggregation pipeline to create monthly plans endpoint

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -8,12 +8,14 @@ const {
   deleteTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require("../controllers/tourController");
 
 const router = express.Router();
 
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 router.route("/tour-stats").get(getTourStats);
+router.route("/monthly-plans/:year").get(getMonthlyPlan);
 
 router.route("/").get(getAllTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);

1afe764bf4a60cf839d8d9c17e6c5e768ab6bbc9
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-09-13 21:53:53 +0600

    implemented error handling for unhandled routes

--- app.js
+++ app.js
@@ -14,4 +14,12 @@ app.use(express.json());
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+app.all("*", (req, res) => {
+  console.log(req.url, req.method);
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} for ${req.method} method on the server!.`,
+  });
+});
+
 module.exports = app;

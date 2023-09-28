76b985d750cf68504e9bde83661323254b0b40dd
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-09-28 16:13:54 +0600

    created reviews model

---
+++ models/reviewModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, "Please provide a review"],
+      trim: true,
+      minLength: [3, "A review must have 3 or more characters"],
+      maxLength: [300, "A review can not be grater then 300 characters"],
+    },
+    rating: {
+      type: Number,
+      min: [1, "Rating should be more then or equal 1"],
+      max: [5, "Rating can not be more then 5"],
+      default: 4,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "A review must have an author"],
+    },
+    tour: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tour",
+      required: [true, "A review should belong to a tour"],
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
+
+const Review = mongoose.model("review", reviewSchema);
+
+module.exports = Review;
